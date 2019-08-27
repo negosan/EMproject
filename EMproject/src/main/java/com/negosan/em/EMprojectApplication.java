8ba9e620a7dad5034193b7a4068f464cfa9cfeba
negosan <425569885@qq.com>
2019-08-27 11:22:43 +0800

    项目环境搭建

---
+++ EMproject/src/main/java/com/negosan/em/EMprojectApplication.java
@@ -0,0 +1,13 @@
+package com.negosan.em;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EMprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EMprojectApplication.class, args);
+	}
+
+}
---
+++ EMproject/src/test/java/com/negosan/em/EMprojectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.negosan.em;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EMprojectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
